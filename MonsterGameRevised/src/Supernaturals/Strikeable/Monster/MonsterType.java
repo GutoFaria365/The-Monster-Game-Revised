4d2a8f546b1ad31d3b2035eae1744a6fa029ac69
KaffaJaffa <rafael.lopes@mindswap.academy>
2023-01-23 16:10:52 +0000

    ENUM

--- MonsterGameRevised/src/Supernaturals/Strikeable/Monster/MonsterType.java
+++ MonsterGameRevised/src/Supernaturals/Strikeable/Monster/MonsterType.java
@@ -1,4 +1,19 @@
 package Supernaturals.Strikeable.Monster;
 
 public enum MonsterType {
+    MUMMY ("Mummy", 30, 80, false),
+    VAMPIRE ("Vampire", 20, 100, false),
+    WEREWOLF ("Werewolf", 20, 120, false);
+
+    private String name;
+    private int attackPower;
+    private int health;
+    private boolean isDead;
+
+    MonsterType(String name, int attackPower, int health, boolean isDead) {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.health = health;
+        this.isDead = isDead;
+    }
 }
