208863b3ac668c97b52f385cf993564da8421854
GutoFaria365 <augusto.filipefaria@gmail.com>
2023-01-23 16:24:13 +0000

    Package

--- MonsterGameRevised/src/GameEngine/Main.java
+++ MonsterGameRevised/src/GameEngine/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("-----MONSTER MASH-----");
         Game game1 = new Game();
         game1.gameStart();
+        System.out.println("teste");
     }
 }
